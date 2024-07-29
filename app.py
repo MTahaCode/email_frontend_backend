e0a861e471a494ae3a68ce5f5bc71c66cfef5230
M Taha Makhdoom <taha.makhdoom10@gmail.com>
2024-07-29 22:58:08 +0500

    add apis keys and nltk

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, request, jsonify
+import os
+import nltk
+import google.generativeai as genai
 
 app = Flask(__name__)
+
+# gemini api key to env
+os.environ["GENERATIVE_AI_API_KEY"] = "AIzaSyBBTYcBb6ZtsFPZEvNTQ7gVqTv7w5MyF_8"
+genai.configure(api_key=os.environ["GENERATIVE_AI_API_KEY"])
+
+# unsplash relted
+nltk.download("punkt")
+nltk.download("stopwords")
+UNSPLASH_ACCESS_KEY = "hnQZn2r_mww-jeUNtkRtIHk9m-Kf-YkghOKQCpWF6qk"
+UNSPLASH_API_URL = "https://api.unsplash.com/search/photos"
 
 @app.route("/")
 def index():
