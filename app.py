d575383bd7cfd11ea02c2240e1530d306d7cc880
M Taha Makhdoom <taha.makhdoom10@gmail.com>
2024-07-29 22:46:47 +0500

    added rake and removed llama comments

--- app.py
+++ app.py
@@ -12,20 +12,11 @@ import google.generativeai as genai
 os.environ["GENERATIVE_AI_API_KEY"] = "AIzaSyBBTYcBb6ZtsFPZEvNTQ7gVqTv7w5MyF_8"
 genai.configure(api_key=os.environ["GENERATIVE_AI_API_KEY"])
 
-# Authenticate with Hugging Face
-# os.environ["HUGGINGFACE_TOKEN"] = "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"
-
 # unsplash relted
 nltk.download("punkt")
 nltk.download("stopwords")
 UNSPLASH_ACCESS_KEY = "hnQZn2r_mww-jeUNtkRtIHk9m-Kf-YkghOKQCpWF6qk"
 UNSPLASH_API_URL = "https://api.unsplash.com/search/photos"
-
-# Load the Meta-Llama model
-# model_id = "meta-llama/Meta-Llama-3-8B"
-# tokenizer = AutoTokenizer.from_pretrained(model_id, use_auth_token=os.environ["HUGGINGFACE_TOKEN"])
-# model = AutoModelForCausalLM.from_pretrained(model_id, use_auth_token=os.environ["HUGGINGFACE_TOKEN"])
-# llama_pipeline = pipeline('text-generation', model=model, tokenizer=tokenizer)
 
 port=5000
 
@@ -46,7 +37,7 @@ def query():
 
         query_text = data['query']
 
-        # Define the prompt for Meta-Llama
+        # Define the prompt for Gemini
         template_prompt = f"""You are a highly intelligent and professional email writer designed to understand user intent related to email text generation. The user provides you with the description of his email and you need to generate the subject, catchy promotion line and somewhat brief but persuasive description pitching the product the user has mentioned in his query.
 
         Make sure that description is (max 60 words), promo is (max 15 words), and subject (max 5 words).
@@ -58,18 +49,12 @@ def query():
         "description": "[A description of the product, make it sound like a salesman promoting and describing his product in a professional way]"
         """
 
-        # # Generate content from the prompt
-        # response = llama_pipeline(template_prompt, max_length=150, num_return_sequences=1)
-        # response_text = response[0]['generated_text']
-
         # Initialize the Gemini model
         model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
         # Generate content from the prompt
         response = model.generate_content(template_prompt)
         response_text = response.text
-
-        # print("Raw response from Meta-Llama:", response_text)  # Print the raw response
 
         # Extract Subject, Promo, and Description from the response
         subject_start = response_text.find('"subject"') + len('"subject"')
